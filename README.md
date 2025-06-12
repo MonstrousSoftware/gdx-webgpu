bd355e344af600efe0d20ac7986bad123e77d9c9
Monstrous <49096535+MonstrousSoftware@users.noreply.github.com>
2025-06-12 22:18:25 +0200

    documentation

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # gdx-webgpu
 
+LibGDX extension based on the WebGPU graphics API instead of OpenGL.
+
+Work in progress.... 
+
+
+
+Currently, uses Java-to-WebGPU as underlying API and only works on Windows desktop using the Dawn library.
+
+
 
--- demo/src/main/java/com/monstrous/HelloTriangle.java
+++ demo/src/main/java/com/monstrous/HelloTriangle.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2025 Monstrous Software.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,10 +24,12 @@ import org.lwjgl.Version;
 import java.io.IOException;
 import java.io.InputStream;
 
-// Render a triangle in a window
-//
-// Following the tutorial "WebGPU in C++" https://eliemichel.github.io/LearnWebGPU/
-
+/** Render a triangle in a window
+ *
+ * Following the tutorial "WebGPU in C++" <a href="https://eliemichel.github.io/LearnWebGPU/">...</a>
+ * This uses "raw" WebGPU functions via the Java-to-WebGPU API and does not use
+ * any libgdx classes.
+ */
 
 public class HelloTriangle {
     public final static int WIDTH = 640;
