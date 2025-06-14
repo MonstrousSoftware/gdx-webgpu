e2ad61fe3963a1a41db05e681526b647f7bb8cd9
Monstrous <49096535+monstroussoftware@users.noreply.github.com>
2025-06-14 11:54:53 +0200

    fixed access to Mesh vertices

--- gdx-webgpu/src/main/java/com/badlogic/gdx/webgpu/graphics/WebGPUMesh.java
+++ gdx-webgpu/src/main/java/com/badlogic/gdx/webgpu/graphics/WebGPUMesh.java
@@ -11,6 +11,13 @@ import com.badlogic.gdx.graphics.glutils.*;
 
 public class WebGPUMesh extends Mesh {
 
+    protected VertexData vertices;  // we need to shadow Mesh.vertices which is package private
+
+    protected WebGPUMesh(VertexData vertices, IndexData indices, boolean isVertexArray) {
+        super(vertices, indices, isVertexArray);
+        this.vertices = vertices;
+    }
+
     /** Creates a new Mesh with the given attributes.
      *
      * @param isStatic whether this mesh is static or not. Allows for internal optimizations.
@@ -19,9 +26,8 @@ public class WebGPUMesh extends Mesh {
      * @param attributes the {@link VertexAttribute}s. Each vertex attribute defines one property of a vertex such as position,
      *           normal or texture coordinate */
     public WebGPUMesh (boolean isStatic, int maxVertices, int maxIndices, VertexAttribute... attributes) {
-        super(new WebGPUVertexData(maxVertices,new VertexAttributes(attributes)), new WebGPUIndexData(maxIndices), false);
+        this(new WebGPUVertexData(maxVertices,new VertexAttributes(attributes)), new WebGPUIndexData(maxIndices), false);
     }
-
 
     /** Creates a new Mesh with the given attributes.
      *
@@ -31,7 +37,7 @@ public class WebGPUMesh extends Mesh {
      * @param attributes the {@link VertexAttributes}. Each vertex attribute defines one property of a vertex such as position,
      *           normal or texture coordinate */
     public WebGPUMesh (boolean isStatic, int maxVertices, int maxIndices, VertexAttributes attributes) {
-        super(new WebGPUVertexData(maxVertices, attributes), new WebGPUIndexData(maxIndices), false);
+        this(new WebGPUVertexData(maxVertices, attributes), new WebGPUIndexData(maxIndices), false);
     }
 
     /** Creates a new Mesh with the given attributes. Adds extra optimizations for dynamic (frequently modified) meshes.
@@ -45,7 +51,7 @@ public class WebGPUMesh extends Mesh {
      *
      * @author Jaroslaw Wisniewski <j.wisniewski@appsisle.com> **/
     public WebGPUMesh (boolean staticVertices, boolean staticIndices, int maxVertices, int maxIndices, VertexAttributes attributes) {
-        super(new WebGPUVertexData(maxVertices, attributes), new WebGPUIndexData(maxIndices), false);
+        this(new WebGPUVertexData(maxVertices, attributes), new WebGPUIndexData(maxIndices), false);
     }
 
     /** Creates a new Mesh with the given attributes. This is an expert method with no error checking. Use at your own risk.
@@ -57,7 +63,7 @@ public class WebGPUMesh extends Mesh {
      * @param attributes the {@link VertexAttribute}s. Each vertex attribute defines one property of a vertex such as position,
      *           normal or texture coordinate */
     public WebGPUMesh (VertexDataType type, boolean isStatic, int maxVertices, int maxIndices, VertexAttribute... attributes) {
-        super(new WebGPUVertexData(maxVertices,new VertexAttributes(attributes)), new WebGPUIndexData(maxIndices), false);
+        this(new WebGPUVertexData(maxVertices,new VertexAttributes(attributes)), new WebGPUIndexData(maxIndices), false);
     }
 
     /** Creates a new Mesh with the given attributes. This is an expert method with no error checking. Use at your own risk.
@@ -68,42 +74,28 @@ public class WebGPUMesh extends Mesh {
      * @param maxIndices the maximum number of indices this mesh can hold
      * @param attributes the {@link VertexAttributes}. */
     public WebGPUMesh (VertexDataType type, boolean isStatic, int maxVertices, int maxIndices, VertexAttributes attributes) {
-        super(null, null, false);
-        throw new RuntimeException("Use another constructor for WebGPUMesh");
+        this(new WebGPUVertexData(maxVertices, attributes), new WebGPUIndexData(maxIndices), false);
     }
 
-
-//    public VertexData getVertexData(){
-//        return vertices;
-//    }
-
+    public VertexData getVertexData(){
+        return vertices;
+    }
 
     @Override
     public void render (ShaderProgram shader, int primitiveType, int offset, int count, boolean autoBind){
-        //....
-        Gdx.app.log("WebGPUMesh", "render() (ignored)");
-        // Set vertex buffer while encoding the render pass
-        // use an offset to set the vertex buffer for this batch
+        Gdx.app.error("WebGPUMesh", "render(ShaderProgram shader, ....) (ignored)");
     }
 
     /** New method for WebGPU rendering (uses a renderPass).
      * note: primitiveType is ignored. Should be set in the pipeline.
      */
     public void render (WebGPURenderPass renderPass, int primitiveType, int offset, int size, int numInstances, int firstInstance){
-        //....
-        //Gdx.app.log("WebGPUMesh", "render(renderPass)");
-
-        // bind indices
-        if( getIndexData() != null)
-            ((WebGPUIndexData)getIndexData()).bind(renderPass);
 
         // bind vertices
-        // HACK: we had to make Mesh.vertices protected for this
-   //     ((WebGPUVertexData)vertices).bind(renderPass);  todo!!!!!
+        ((WebGPUVertexData)vertices).bind(renderPass);
 
-        //renderPass.setBindGroup( 0, bg.getHandle(), 0, JavaWebGPU.createNullPointer());
-
-        if( getIndexData() != null) {
+        if( getIndexData() != null) {   // is it an indexed mesh?
+            ((WebGPUIndexData)getIndexData()).bind(renderPass);// bind indices
             renderPass.drawIndexed(size, numInstances, offset, 0, firstInstance);
         } else {
             renderPass.draw(size, numInstances, offset, firstInstance);
