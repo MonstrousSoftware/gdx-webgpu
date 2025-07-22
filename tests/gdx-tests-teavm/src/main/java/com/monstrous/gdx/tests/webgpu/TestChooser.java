2321be4c52b84aa05adb97d938e570dc9da4e176
Natan <xpenatan@gmail.com>
2025-07-22 20:04:27 -0300

    Remove touch down input

--- tests/gdx-tests-teavm/src/main/java/com/monstrous/gdx/tests/webgpu/TestChooser.java
+++ tests/gdx-tests-teavm/src/main/java/com/monstrous/gdx/tests/webgpu/TestChooser.java
@@ -44,16 +44,6 @@ public class TestChooser extends ApplicationAdapter {
                 }
                 return false;
             }
-
-            @Override
-            public boolean touchDown (int screenX, int screenY, int pointer, int button) {
-                if (screenX < Gdx.graphics.getWidth() / 10.0 && screenY < Gdx.graphics.getHeight() / 10.0) {
-                    if (test != null) {
-                        dispose = true;
-                    }
-                }
-                return false;
-            }
         };
 
         ((InputWrapper)Gdx.input).multiplexer.addProcessor(stage);
