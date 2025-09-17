37efbace375c07d48881b2db903a0f780b9cbce3
Monstrous <49096535+MonstrousSoftware@users.noreply.github.com>
2025-09-17 18:12:31 +0200

    modified renderable sorter

--- gdx-webgpu/src/main/java/com/monstrous/gdx/webgpu/application/WgVersion.java
+++ gdx-webgpu/src/main/java/com/monstrous/gdx/webgpu/application/WgVersion.java
@@ -3,6 +3,6 @@ package com.monstrous.gdx.webgpu.application;
 public class WgVersion {
 
     public static String getVersion(){
-        return "gdx-webgpu v0.3-snapshot";
+        return "gdx-webgpu v0.3-snapshot 17/09/2025 18:12";
     }
 }
