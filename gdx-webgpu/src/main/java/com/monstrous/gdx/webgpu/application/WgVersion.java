ede2c36199fee638852a7e7b0eb1d107159bf337
Monstrous <49096535+MonstrousSoftware@users.noreply.github.com>
2025-09-29 18:19:35 +0200

    v0.3

--- gdx-webgpu/src/main/java/com/monstrous/gdx/webgpu/application/WgVersion.java
+++ gdx-webgpu/src/main/java/com/monstrous/gdx/webgpu/application/WgVersion.java
@@ -3,6 +3,6 @@ package com.monstrous.gdx.webgpu.application;
 public class WgVersion {
 
     public static String getVersion(){
-        return "gdx-webgpu v0.3-snapshot 29/09/2025";
+        return "gdx-webgpu v0.3";
     }
 }
