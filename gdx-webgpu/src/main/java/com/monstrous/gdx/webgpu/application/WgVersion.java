544a36f7e3d0c959753e5bcbf4f913fbd1cc15fc
Monstrous <49096535+MonstrousSoftware@users.noreply.github.com>
2025-12-01 22:25:53 +0100

    merged point sprite particles

--- gdx-webgpu/src/main/java/com/monstrous/gdx/webgpu/application/WgVersion.java
+++ gdx-webgpu/src/main/java/com/monstrous/gdx/webgpu/application/WgVersion.java
@@ -3,6 +3,6 @@ package com.monstrous.gdx.webgpu.application;
 public class WgVersion {
 
     public static String getVersion() {
-        return "gdx-webgpu v0.7.4-snapshot Nov 26, 2025";
+        return "gdx-webgpu v0.7.4-snapshot Dec 1, 2025";
     }
 }
