6364c5e693d11d927d840f27dcfc759bf6b38fa8
Natan <xpenatan@gmail.com>
2026-01-17 22:52:23 -0300

    Add jitpack support

--- buildSrc/src/main/kotlin/LibExt.kt
+++ buildSrc/src/main/kotlin/LibExt.kt
@@ -2,16 +2,22 @@ import java.io.File
 import java.util.Properties
 
 object LibExt {
-    const val groupId = "io.github.monstroussoftware.gdx-webgpu"
+    var overrideGroup: String? = null
+    val groupId get() = overrideGroup ?: "io.github.monstroussoftware.gdx-webgpu"
     const val libName = "gdx-webgpu"
+    var overrideVersion: String? = null
     var libVersion: String = ""
         get() {
-            return getVersion()
+            return overrideVersion ?: getVersion()
         }
     var isRelease = true
 }
 
 private fun getVersion(): String {
+    val propVersion = System.getProperty("version")
+    if (propVersion != null) {
+        return propVersion
+    }
     var libVersion = "-SNAPSHOT"
     val file = File("gradle.properties")
     if(file.exists()) {
