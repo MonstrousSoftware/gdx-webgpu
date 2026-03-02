f3cadac462f5067450b5aee490d08ab880f0c5a1
Natan <xpenatan@gmail.com>
2026-03-01 22:00:07 -0300

    Update min sdk to 29

--- tests/gdx-tests-android/build.gradle.kts
+++ tests/gdx-tests-android/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.monstrous.gdx.tests.webgpu"
-        minSdk = 26
+        minSdk = 29
         versionCode = 1
         versionName = "0.0.1"
     }
