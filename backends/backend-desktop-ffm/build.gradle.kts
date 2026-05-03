df6f5ba8e6a293570d095f120998ea6bc2d8e2ed
Natan <xpenatan@gmail.com>
2026-05-03 11:21:18 -0300

    Update api instead

--- backends/backend-desktop-ffm/build.gradle.kts
+++ backends/backend-desktop-ffm/build.gradle.kts
@@ -18,7 +18,7 @@ dependencies {
 
     api(project(":backends:backend-desktop"))
     api("com.github.xpenatan.jWebGPU:webgpu-ffm:$jWebGPUVVersion")
-    runtimeOnly("com.github.xpenatan.jWebGPU:webgpu-ffm:$jWebGPUVVersion:desktop")
+    api("com.github.xpenatan.jWebGPU:webgpu-ffm:$jWebGPUVVersion:desktop")
 }
 
 publishing {
